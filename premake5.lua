c82e323e6b8dfdc3b2144404e57242e4481aa1b2
jaguan <45917378+jaguan@users.noreply.github.com>
2020-02-20 00:59:05 +0900

    Create premake5.lua
    for visual studio project

---
+++ premake5.lua
@@ -0,0 +1,28 @@
+project "ImGui"
+    kind "StaticLib"
+    language "C++"
+  
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("obj/" .. outputdir .. "/%{prj.name}")
+    
+    files
+    {
+        "imconfig.h",
+        "imgui.h",
+        "imgui.cpp",
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+        "imgui_widgets.cpp",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h",
+        "imgui_demo.cpp"
+    }
+    
+    filter "system:windows"
+        cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.17763.0"
+        
+    filter { "system:windows", "configurations:Release" }
+        buildoptions "/MT"
